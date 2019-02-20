ca120a9cccd82603d57a385176e02d8225f35cd6
Alok Pandey <alokrocks217@gmail.com>
2019-02-20 21:07:46 +0530

    Added Encrytion and Decryption of files

--- server/decryptFolder.js
+++ server/decryptFolder.js
@@ -3,10 +3,10 @@ var decryptor = require('file-encryptor');
 
 function dec(outputfile,inputfile,key,options){
 
-decryptor.decryptFile(outputfile, inputfile, key, options, function(err) {
- 	if (err) throw err;
- 	console.log('Successfully decrypted');
- });
+	decryptor.decryptFile(outputfile, inputfile, key, options, function(err) {
+	 	if (err) throw err;
+	 	console.log('Successfully decrypted');
+	 });
 }
 
 module.exports= { dec };
